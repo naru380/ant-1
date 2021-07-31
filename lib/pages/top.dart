fab028e411b10b89b006ce5df9d93a0591221c95
taiyo78910 <y.kambayashi78910@gmail.com>
2021-07-31 17:52:49 +0900

    #11 カメラロールから選択してもクラッシュしないように修正

--- lib/pages/top.dart
+++ lib/pages/top.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:provider/provider.dart';
+import 'package:camera/camera.dart';
 import 'play.dart';
 import 'dart:async';
 import 'dart:io';
@@ -80,7 +81,7 @@ class _TopScreenState extends State<TopScreen> {
             icon: Icon(Icons.settings),
             onPressed: () async {
               //Navigator.of(context).pushNamed('/setting');
-              
+
               // temporary change on  branch feature/#4
               // TODO: create link for game-play page.
               context.read<PuzzleProvider>().init();
